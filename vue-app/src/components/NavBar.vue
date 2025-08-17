eb5f9a915fc82164296a72a146b755fdb8128b77
AlvaroCuti <alvarocutillas9@gmail.com>
2025-08-17 10:04:14 +0200

    Cambios esteticos

--- vue-app/src/components/NavBar.vue
+++ vue-app/src/components/NavBar.vue
@@ -2,13 +2,41 @@
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="barra">
+    <div class="item">
+        <button>
+            <img src="@/assets/menu.png" width="30" height="30"/>
+        </button>
+    </div>
+    <div class="item">
+        <h3>My Weather App</h3>
+    </div>
+    <div class="item">
+        <button>
+            <img src="@/assets/plus.png" width="30" height="30"/>
+        </button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<style scoped>
+  .barra{
+    position: fixed;
+    display: flex;
+    top: 0;            /* Desde la parte superior */
+    left: 0;           /* Desde la izquierda */
+    width: 100%;       /* Ocupa todo el ancho */
+    justify-content: space-between;
+    align-items: center;
+    background-color: rgb(78, 169, 226);
+  }
+
+  .item{
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  button{
+    background-color: rgb(78, 169, 226);;
+  }
+</style>
---
+++ vue-app/src/components/WeatherPlace.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="completo">
+   
+  </div>
+</template>
+
+<style scoped>
+  .completo{
+    background-color: antiquewhite;
+    position: fixed;
+    top: 0;            /* Desde la parte superior */
+    left: 0;           /* Desde la izquierda */
+    width: 100%;       /* Ocupa todo el ancho */
+  }
+</style>
